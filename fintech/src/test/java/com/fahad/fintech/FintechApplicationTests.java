720556d8ee925e5bb75dfd2b387448effddc21cf
kokfahad <77542699+kokfahad@users.noreply.github.com>
2022-08-16 09:32:38 +0600

    project initialized

---
+++ fintech/src/test/java/com/fahad/fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahad.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
